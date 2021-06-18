a4a13c1b40d82e04314971046f3305fcfad28e00
hadikp <hp20001025@gmail.com>
2021-06-18 17:21:39 +0200

    http

--- nodejs-alapjai-http-modul/server.js
+++ nodejs-alapjai-http-modul/server.js
@@ -3,13 +3,13 @@ const SiteRouter = require('./routers/site.router')
 
 const port = 8080
 
-http.createServer(({ url }, res) => {
-    if(url){
-        let now = new Date()
-        console.log(now.toLocaleDateString('hu'),url)
-    }
-  SiteRouter[url]
-    ? SiteRouter[url](res)
+http.createServer((req, res) => {
+  if (req.url) {
+    let now = new Date()
+    console.log(`Date: ${now.toLocaleDateString('hu')}, Url: ${req.url}, Method: ${req.method}`)
+  }
+  SiteRouter[req.url]
+    ? SiteRouter[req.url](res)
     : SiteRouter['/404'](res)
 }).listen(port)
 
