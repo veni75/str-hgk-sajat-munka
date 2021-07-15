bfd960ef26e2ceb548a4e32c628a8d04f761b8c4
veni75 <vardai.eniko@gmail.com>
2021-07-15 19:41:13 +0200

    feladat3

---
+++ api-feladat-03/src/controllers/person/routes.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const data = require('./data');
+const createError = require('http-errors');
+
+const controller = express.Router();
+
+controller.get('/count', (req, res) => {
+    res.json(data.length);
+});
+
+controller.get('/vaccinated', (req, res) => {
+    let result = data.filter(dat => dat.vaccine !== "");
+    res.json(result);
+});
+
+//1.
+controller.get('/:id/vaccinated', (req, res, next) => {
+    const person = data.find(pers => pers.id === Number(req.params.id));
+    if (!person) {
+        return next(new createError.NotFound("Person is not found"));
+    }
+    res.json(person.vaccine);
+});
+
+// 2. Create a new person.
+controller.post('/', (req, res, next) => {
+    const { lastName, firstName, vaccine } = req.body;
+    if (!lastName || !firstName || !vaccine) {
+        return next(
+            new createError.BadRequest("Missing properties!")
+        );
+    }
+
+    const newPerson = req.body;
+    newPerson.id = data[data.length - 1].id + 1;
+    data.push(newPerson);
+    res.status(201);
+    res.json(newPerson);
+});
+
+// 3. Update a person.
+controller.put('/:id/:vaccine', (req, res, next) => {
+    const id = req.params.id;
+    const vaccine = req.params.vaccine;
+    const index = data.findIndex(p => p.id === Number(id));
+    const { firstName, lastName } = data[index];
+
+    if (!lastName || !firstName || !vaccine) {
+        return next(
+            new createError.BadRequest("Missing properties!")
+        );
+    }
+
+    data[index] = {
+        id,
+        firstName,
+        lastName,
+        vaccine
+    };
+
+    res.json(data);
+});
+
+// 4. Delete
+controller.delete('/:vaccine', (req, res,next) => {
+    const vaccina = req.params.vaccine;
+    if (!vaccina) {
+        return next(new createError.NotFound("Vaccine is not found"));
+    }
+    let result = data.filter(p => p.vaccine !== vaccina);
+    res.json(result);
+});
+
+module.exports = controller;
---
+++ api-feladat-03/src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 3000;
+
+app.use(bodyParser.json());
+
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./docs/swagger.yaml');
+app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerDocument));
+
+app.use('/person',require('./controllers/person/routes'));
+
+app.listen(port,()=>{
+    console.log(`App listening at http://localhost:${port}`);
+})
